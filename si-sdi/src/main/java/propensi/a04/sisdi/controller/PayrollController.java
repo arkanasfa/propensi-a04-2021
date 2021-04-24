67454e4f61b49370cf616ca6248346a8b5b9f36f
amilahzahiroh <amilahzahiroh@gmail.com>
2021-04-24 15:34:36 +0700

    Payroll,Notif,Popup

--- si-sdi/src/main/java/propensi/a04/sisdi/controller/PayrollController.java
+++ si-sdi/src/main/java/propensi/a04/sisdi/controller/PayrollController.java
@@ -1,32 +1,24 @@
 package propensi.a04.sisdi.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import propensi.a04.sisdi.model.DokumenTotalModel;
 import propensi.a04.sisdi.model.GajiModel;
 import propensi.a04.sisdi.model.KomponenPengaliModel;
+import propensi.a04.sisdi.repository.KomponenPengaliDb;
+import propensi.a04.sisdi.service.PayrollService;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/payroll")
 public class PayrollController {
-//    @Autowired
-//    UserService userService;
-//
-//    @Autowired
-//    LemburService lemburService;
-//
-//    @Autowired
-//    GajiService gajiService;
+    @Autowired
+    PayrollService payrollService;
+
+    @Autowired
+    KomponenPengaliDb komponenPengaliDb;
 
     @GetMapping("")
     private String requestPayrollForm(Model model) {
@@ -46,9 +38,32 @@ public class PayrollController {
         return "detail-gaji";
     }
 
+    // View Komponen Gaji
     @GetMapping("/komponenGaji")
     private String komponenGaji(Model model){
-        model.addAttribute("komponenGaji", new KomponenPengaliModel());
+        KomponenPengaliModel komponenGaji = payrollService.getKomponenGajiById(Long.parseLong("1"));
+        model.addAttribute("komponenGaji", komponenGaji);
         return"komponen-pengali-gaji";
+    }
+
+    // Form Ubah Komponen Gaji
+    @GetMapping("/komponenGaji/ubah")
+    private String ubahKomponenGajiForm(
+            @RequestParam(value="id") Long id,
+            Model model) {
+        KomponenPengaliModel komponenGaji = payrollService.getKomponenGajiById(id);
+        model.addAttribute("komponenGaji", komponenGaji);
+        return "komponen-pengali-gaji-form";
+    }
+
+    // Notifikasi Ubah Komponen Gaji
+    @RequestMapping(value="/komponenGaji/ubah", method = RequestMethod.POST)
+    private String ubahKomponenGajiSubmit(
+            @ModelAttribute KomponenPengaliModel komponenGaji,
+            Model model) {
+        payrollService.ubahKomponenGaji(komponenGaji);
+        model.addAttribute("komponenGaji", komponenGaji);
+        model.addAttribute("kode", "");
+        return "payroll-notifs";
     }
 }
