ec358f08e5828511a665cf83165185f125f44067
arkan asfa <arkanasfa@gmail.com>
2021-05-17 12:34:52 +0700

    backup arkan

--- si-sdi/src/main/java/propensi/a04/sisdi/repository/KaryawanDB.java
+++ si-sdi/src/main/java/propensi/a04/sisdi/repository/KaryawanDB.java
@@ -2,14 +2,17 @@ package propensi.a04.sisdi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import propensi.a04.sisdi.model.GajiModel;
 import propensi.a04.sisdi.model.KaryawanModel;
 import propensi.a04.sisdi.model.UserModel;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface KaryawanDB extends JpaRepository<KaryawanModel, Integer> {
-    KaryawanModel findByUser(UserModel user);
-    Optional<KaryawanModel> findById(Long id_karyawan);
+public interface KaryawanDb extends JpaRepository<KaryawanModel,Long> {
+    List<KaryawanModel> findAll();
+    //KaryawanModel findByUser(UserModel user);
+    Optional<KaryawanModel> findByGajiModel(GajiModel gaji);
     Optional<KaryawanModel> findByNoKaryawan(Long no_karyawan);
 }
-
